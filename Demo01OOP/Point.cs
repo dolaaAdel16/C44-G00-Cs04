ac82ef9d9cf2833441b0cc399ecfd30f088c5fd6
Ahmed <aa3452000@gmail.com>
2025-07-20 07:53:14 +0300

    V01

---
+++ Demo01OOP/Point.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01OOP
+{
+    internal struct Point
+    {
+        public int x = default;
+        public int y = default;
+
+
+        //public Point()
+        //{
+
+        //}
+
+        //public Point()
+        //{
+        //    x = 12;
+        //    y = 12; 
+        
+        //}
+        public Point(int X , int Y)
+        {
+            x = X;
+            y = Y; 
+
+        }
+
+        public void PrintPoint()
+        { 
+            Console.WriteLine($"({x}, {y})");
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+    }
+}
--- Demo01OOP/Program.cs
+++ Demo01OOP/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("V01");
+
+            Point P01;
+
+            //Console.WriteLine(P01.x);
+            //Console.WriteLine(P01.y);
+
+            //P01.x = 12;
+            //P01.y = 12;
+
+            //Console.WriteLine(P01.x);
+            //Console.WriteLine(P01.y);
+
+            P01 = new Point(1,2); 
+
+            //Console.WriteLine(P01.x);   
+            //Console.WriteLine(P01.y);   
+
+            //Console.WriteLine(P01);
+
+            //P01.PrintPoint();
+
+            Console.WriteLine(P01); 
+
         }
     }
 }
